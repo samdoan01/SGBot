e087151cc57f8fff7fec68ae4db7734a0f732e21
samdoan <samdoan01@gmail.com>
2018-11-19 15:31:46 -0600

    set up function to retrieve bible verse from reference

--- index.js
+++ index.js
@@ -23,11 +23,64 @@ bot.on('start', () => {
 	params
 	);
 	
+});
+
+// ERROR Handling
+bot.on('error', (err) => console.log(err));
+
+// Message Handling
+bot.on('message', data => {
+	if(data.type !== 'message') {
+		return;
+	}
+	
+	handleMessage(data.text);
+});
+
+// Respond to Data
+function handleMessage(message) {
+	if(message.includes(' chucknorris')) {
+		chuckJoke();
+	}
+	else if(message.matches('(?:\d\s*)?[A-Z]?[a-z]+\s*\d+(?:[:-]\d+)?(?:\s*-\s*\d+)?(?::\d+|(?:\s*[A-Z]?[a-z]+\s*\d+:\d+))?')) {
+		getBibleVerse();
+	}
 }
 
-	
-	
-	
-);
 
+// Tell a Chuck Norris Joke
+function chuckJoke() {
+	axios.get('http://api.icndb.com/jokes/random')
+	.then(res => {
+		const joke = res.data.value.joke;
+	
+		const params = {
+				icon_emoji: ':laughing:'
+		};
+		
+		bot.postMessageToChannel(
+		'general',
+		`Chuck Norris: ${joke}`,
+		params
+		);
+	});
+
+// Retrieve bible verse; if exists 
+function getBibleVerse() {
+	axios.get('http://api.icndb.com/jokes/random')
+	.then(res => {
+		const joke = res.data.value.joke;
+	
+		const params = {
+				icon_emoji: ':laughing:'
+		};
+		
+		bot.postMessageToChannel(
+		'general',
+		`Chuck Norris: ${joke}`,
+		params
+		);
+	});
+}
+}
 
