e94e07f606e7eb859ce3a073c278700f59573034
samdoan <samdoan01@gmail.com>
2018-11-20 12:56:33 -0600

    prevented long queries from being printed

--- index.js
+++ index.js
@@ -38,8 +38,14 @@ bot.on('message', data => {
 
 // Respond to Data
 function handleMessage(message) {
-	//var regex = new RegExp(/(?:\d\s*)?[A-Z]?[a-z]+\s*\d+(?:[:-]\d+)?(?:\s*-\s*\d+)?(?::\d+|(?:\s*[A-Z]?[a-z]+\s*\d+:\d+))?/);
-	var regex = new RegExp(/(?:\d\s*)?[A-Z]?[a-z]+\s*\d+(?::\d+|(?:\s*[A-Z]?[a-z]+\s*\d+:\d+))?/);
+	
+	// /(?:\d\s*)?     -> 1/2
+	// [A-Z]?[a-z]+\s* -> Letter and spaces
+	// \d+             -> Chapter number
+	// (?:[:-]\d+)?    -> Additional chapters
+	// (?:\s*-\s*\d+)? -> Spaces and numbers
+	var regex = new RegExp(/(?:\d\s*)?[A-Z]?[a-z]+\s*\d+(?:[:-]\d+)?(?:\s*-\s*\d+)?(?::\d+|(?:\s*[A-Z]?[a-z]+\s*\d+:\d+))?/);
+	//var regex = new RegExp(/(?:\d\s*)?[A-Z]?[a-z]+\s*\d(?:\s*-\s*\d+)?(?::\d+|(?:\s*[A-Z]?[a-z]+\s*\d+:\d+))?/);
 	if(regex.test(message) == true) {
 		getBibleVerse(regex.exec(message));
 	}
@@ -48,18 +54,25 @@ function handleMessage(message) {
 
 function getBibleVerse(verse) {
 	var src = 'https://bible-api.com/' + verse;
-	console.log(src);
 	axios.get(src)
+	
 	.then(res => {
+		
 		const text = res.data.text;
 		const reference = res.data.reference;
-		
 		const formattedText = "```".concat(reference).concat("\n").concat(text).concat("```");
-		
-		bot.postMessageToChannel(
-		'general',
-		`${formattedText}`
-		);
+		if(text.length > 4000) {
+			bot.postMessageToChannel(
+					'general',
+					`Ahh...${reference} is too long. Please try a smaller reference.`
+					)
+		}
+		else {
+			bot.postMessageToChannel(
+					'general',
+					`${formattedText}`
+			);
+		}
 	});
 }
 
