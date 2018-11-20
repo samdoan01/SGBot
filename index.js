fea5ac94b3aee34a7a61c568448c05086364ce06
samdoan <samdoan01@gmail.com>
2018-11-20 14:30:35 -0600

    can respond to greetings, thanks, and insults

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
 
+var hasDoneService = false;
+
 const bot = new SlackBot({
 	token: 'xoxb-482580549056-484411099463-j1R8ptANL6ysJXAcix56uEJR',
 	name: 'sgbot'
@@ -42,11 +44,19 @@ function handleMessage(message) {
 	var verseRegex = new RegExp(/(?:\d\s*)?[A-Z]?[a-z]+\s*\d+(?:[:-]\d+)?(?:\s*-\s*\d+)?(?::\d+|(?:\s*[A-Z]?[a-z]+\s*\d+:\d+))?/);
 	var helloRegex = new RegExp(/(H|h)ello|(H|h)i|(W|w)(h?)at('?)s (up|good|gud|gucci)/);
 	var sgbotRegex = new RegExp(/((S|s)(G|g)(B|b)(O|o)(T|t))|@UE8C32XDM/);
+	var thanksRegex = new RegExp(/(T|t)(hanks|hx|y)/);
+	var insultRegex = new RegExp(/((S|s)tupid)|((D|d)um(b?))|((U|u)gly)|((L|l)ame)/);
 	if(verseRegex.test(message) == true) {
 		getBibleVerse(verseRegex.exec(message));
 	}
 	else if(helloRegex.test(message) && sgbotRegex.test(message)) {
 		sayHiBack(message);
+	}
+	else if(thanksRegex.test(message) && sgbotRegex.test(message)) {
+		respondToThanks();
+	}
+	else if(insultRegex.test(message) && sgbotRegex.test(message)) {
+		respondToInsult();
 	}
 
 }
@@ -63,10 +73,11 @@ function getBibleVerse(verse) {
 		if(text.length > 4000) {
 			bot.postMessageToChannel(
 					'general',
-					`Ahh...${reference} is too long. Please try a smaller reference.`
+					`_Ahh...${reference} is too long. Please try a smaller reference._`
 					)
 		}
 		else {
+			hasDoneService = true;
 			bot.postMessageToChannel(
 					'general',
 					`${formattedText}`
@@ -81,6 +92,51 @@ function sayHiBack(message) {
 			'general',
 			`_Hello there!_`
 			);
+}
+
+function respondToThanks() {
+	if(hasDoneService == true) {
+		hasDoneService = false;
+		var rng = Math.floor((Math.random() * 10) + 1);
+		var msg;
+		if(rng < 5) {
+			msg = `_You're very welcome!_`;
+		}
+		else if(rng < 9) {
+			msg = `_It's no problem at all!_`;
+		}
+		else {
+			msg = `_Please venmo Sam Doan if you like this service! (JK)_`;
+		}
+		bot.postMessageToChannel(
+			'general',
+			msg
+			);
+	}
+	else {
+		bot.postMessageToChannel(
+				'general',
+				`_Thanks for what?_`
+				);
+	}
+	
+}
+
+function respondToInsult() {
+	
+	var rng = Math.floor((Math.random() * 10) + 1);
+	var msg;
+	if(rng < 5) {
+		msg = `_Umm...that wasn't very nice._`;
+	}
+	else {
+		msg = `_Hey, c'mon now!_`;
+	}
+	bot.postMessageToChannel(
+		'general',
+		msg	
+	)
+	
 }
 
 
